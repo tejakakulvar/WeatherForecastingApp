a3682110b97040d2ed432fb78a82b68bf4038980
tejakakulvar <teja.kakulvar@gmail.com>
2017-11-21 10:50:13 -0600

    sfs

--- src/components/chartgenerator.js
+++ src/components/chartgenerator.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class chartgenerator extends React.Component{
-  render(){
+  render()
+  {
+    debugger;
+    console.log('here');
     return(
       <div>
         <h1> Hihdahd </h1>
